d71bf781c506ef18d6472b3f9c10503a856d56f7
Jeong-Seunghyun <suaca123@gmail.com>
2022-02-11 13:50:01 +0900

    security 해결중

--- src/main/java/com/mypatch/www/member/dto/MemberDTO.java
+++ src/main/java/com/mypatch/www/member/dto/MemberDTO.java
@@ -1,5 +1,22 @@
 package com.mypatch.www.member.dto;
 
-public class MemberDTO {
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MemberDTO {
+	private String member_id;
+	private String member_name;
+	private String member_pwd;
+	private String member_birth;
+	private char member_alarm;
+	private String member_nick;
+	private List<AuthDTO> authList;
 }
