5ad762632deff3180387f1c9a1beb927bcc5ddbf
ung <ung@localhost>
2022-02-11 14:09:21 +0900

    Board 패키지 3차 수정 ksw 22.02.11 14:09

---
+++ src/main/java/com/mypatch/www/board/domain/BoardAttachDTO.java
@@ -0,0 +1,13 @@
+package com.mypatch.www.board.domain;
+
+import lombok.Data;
+
+@Data
+public class BoardAttachDTO {
+	
+	private String boardattach_uuid;
+	private Integer board_num;
+	private String boardattach_filename;
+	private String boardattach_path;
+
+}
--- src/main/java/com/mypatch/www/board/domain/LikeDTO.java
+++ src/main/java/com/mypatch/www/board/domain/LikeDTO.java
@@ -1,16 +1,12 @@
 package com.mypatch.www.board.domain;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
-public class BoardDTO {
+public class LikeDTO {
 	
 	private Integer board_num;
 	private String member_id;
-	private String board_content;
-	private Date board_date;
-	
-	
+	private char like_cnt;
+
 }
