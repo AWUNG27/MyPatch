1aeceacaa220883184af6f3a8a558e8e737d1039
JIMIN <JIMIN@DESKTOP-H0MBLJA>
2022-02-17 15:03:06 +0900

    20220217 박지민

--- src/main/java/com/mypatch/www/member/mapper/MemberMapper.java
+++ src/main/java/com/mypatch/www/member/mapper/MemberMapper.java
@@ -2,11 +2,11 @@ package com.mypatch.www.member.mapper;
 
 
 import java.util.List;
+import java.util.Map;
 
 import com.mypatch.www.board.domain.BoardDTO;
-
 import com.mypatch.www.member.domain.MemberDTO;
-import java.util.Map;
+import com.mypatch.www.member.domain.ProfileDTO;
 
 public interface MemberMapper {
 	public MemberDTO selectUser(String member_id);
@@ -19,4 +19,10 @@ public interface MemberMapper {
 
 	public void follow(Map<String, String> paramMap);
 	public void unfollow(Map<String, String> paramMap);
+
+	public List<ProfileDTO> followingProfile(String member_id);
+	public List<MemberDTO> followingInfo(String member_id);
+	
+	public List<ProfileDTO> followerProfile(String member_nick);
+	public List<MemberDTO> followerInfo(String member_nick);
 }
--- src/main/java/com/mypatch/www/member/service/IMemberService.java
+++ src/main/java/com/mypatch/www/member/service/IMemberService.java
@@ -1,5 +1,7 @@
 package com.mypatch.www.member.service;
 
+import java.util.List;
+
 import com.mypatch.www.member.domain.MemberDTO;
 
 public interface IMemberService {
@@ -10,4 +12,6 @@ public interface IMemberService {
 	public void follow(String member_id, String member_nick);
 	public void unfollow(String member_nick, String member_id);
 	
+	public List<MemberDTO> following(String member_id);
+	public List<MemberDTO> follower(String member_nick);
 }
