4ecbc2ac17ac447ca7bb2fd7bf70b11ee2448155
cnlguq <cnlguq@gmail.com>
2022-02-17 16:28:29 +0900

    2022-02-17 kjw commit

--- src/main/java/com/mypatch/www/member/MemberControllerkjw.java
+++ src/main/java/com/mypatch/www/member/MemberControllerkjw.java
@@ -1,15 +1,22 @@
 package com.mypatch.www.member;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mypatch.www.board.domain.BoardAttachDTO;
 import com.mypatch.www.board.domain.BoardDTO;
 import com.mypatch.www.member.mapper.MemberMapper;
+import com.mypatch.www.util.DeduplicationUtils;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -32,14 +39,102 @@ public class MemberControllerkjw {
   
 	//내 프로필 페이지 이동
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	public void profile(String member_id) {
+	public void profile(String member_id, Model model, String cnt) {
+		if ("".equals(cnt) || cnt == null) {
+			cnt = "0";
+		}
+		log.info("cnt: " + cnt);
 		List<BoardDTO> bList = new ArrayList<BoardDTO>();
+		List<BoardAttachDTO> bimgList = new ArrayList<BoardAttachDTO>();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		log.info("My page..");
 		log.info("내 아이디: " + member_id);
-		bList = memberMapper.profileread(member_id);
-		for (BoardDTO bDto : bList) {
-			log.info(bDto);
+		bList = memberMapper.profileboard(member_id, cnt);
+		bimgList = memberMapper.profileimg(member_id, cnt);
+		Integer bcnt = memberMapper.profilecnt(member_id);
+		bimgList = DeduplicationUtils.deduplication(bimgList, BoardAttachDTO::getBoard_num);
+		for (BoardAttachDTO boardAttachDTO : bimgList) {
+			log.info("중복 제거" + boardAttachDTO);
 		}
+		//날짜 변환
+		for (BoardDTO bDto : bList) {
+			bDto.setDate(simpleDateFormat.format(bDto.getBoard_date()));
+		}
+		
+		//경로 변환
+		for (BoardAttachDTO bAttachDTO : bimgList) {
+			String path = bAttachDTO.getBoardattach_path();
+			bAttachDTO.setBoardattach_path(path.replaceAll("\\\\", "/"));
+		}
+		
+		//BoardDTO의 bList에 BoardAttachDTO 값을 담기
+		for (BoardDTO bDto : bList) {
+			List<BoardAttachDTO> query = new ArrayList<BoardAttachDTO>();
+			for (BoardAttachDTO bAttachDTO : bimgList) {
+				if (bDto.getBoard_num() == bAttachDTO.getBoard_num()) {
+					query.add(bAttachDTO);
+				}
+			}
+			bDto.setBList(query);
+		}
+		for (BoardDTO allList : bList) {
+			log.info(allList);
+		}
+		model.addAttribute("bList", bList);
+		model.addAttribute("bimgList", bimgList);
+		model.addAttribute("bcnt", bcnt);
 	}
+	//내 프로필 스크롤 이벤트
+	@ResponseBody
+	@RequestMapping(value = "/scroll", method = RequestMethod.GET)
+	public Map<String, Object> scroll(String member_id, Model model, String cnt) {
+		Map<String, Object> profiledata = new HashMap<>();
+		if ("".equals(cnt) || cnt == null) {
+			cnt = "0";
+		}
+		log.info("cnt: " + cnt);
+		List<BoardDTO> bList = new ArrayList<BoardDTO>();
+		List<BoardAttachDTO> bimgList = new ArrayList<BoardAttachDTO>();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		log.info("My page..");
+		log.info("내 아이디: " + member_id);
+		bList = memberMapper.profileboard(member_id, cnt);
+		bimgList = memberMapper.profileimg(member_id, cnt);
+		bimgList = DeduplicationUtils.deduplication(bimgList, BoardAttachDTO::getBoard_num);
+		for (BoardAttachDTO boardAttachDTO : bimgList) {
+			log.info("중복 제거" + boardAttachDTO);
+		}
+		//날짜 변환
+		for (BoardDTO bDto : bList) {
+			bDto.setDate(simpleDateFormat.format(bDto.getBoard_date()));
+		}
+		
+		//경로 변환
+		for (BoardAttachDTO bAttachDTO : bimgList) {
+			String path = bAttachDTO.getBoardattach_path();
+			bAttachDTO.setBoardattach_path(path.replaceAll("\\\\", "/"));
+		}
+		
+		//BoardDTO의 bList에 BoardAttachDTO 값을 담기
+		for (BoardDTO bDto : bList) {
+			List<BoardAttachDTO> query = new ArrayList<BoardAttachDTO>();
+			for (BoardAttachDTO bAttachDTO : bimgList) {
+				if (bDto.getBoard_num() == bAttachDTO.getBoard_num()) {
+					query.add(bAttachDTO);
+				}
+			}
+			bDto.setBList(query);
+		}
+		for (BoardDTO allList : bList) {
+			log.info(allList);
+		}
+		
+		profiledata.put("bList", bList);
+		profiledata.put("bimgList", bimgList);
+		
+		return profiledata;
+	}
+	
+	
 	
 }
--- src/main/java/com/mypatch/www/member/mapper/MemberMapper.java
+++ src/main/java/com/mypatch/www/member/mapper/MemberMapper.java
@@ -3,10 +3,13 @@ package com.mypatch.www.member.mapper;
 
 import java.util.List;
 
+import com.mypatch.www.board.domain.BoardAttachDTO;
 import com.mypatch.www.board.domain.BoardDTO;
 
 import com.mypatch.www.member.domain.MemberDTO;
 import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
 
 public interface MemberMapper {
 	public MemberDTO selectUser(String member_id);
@@ -15,7 +18,9 @@ public interface MemberMapper {
 	public int insertMember(MemberDTO mDto);
 	public int insertMemberAuth(String member_id);
 	
-	public List<BoardDTO> profileread(String member_id);
+	public List<BoardAttachDTO> profileimg(@Param("member_id") String member_id,@Param("cnt") String cnt);
+	public List<BoardDTO> profileboard(@Param("member_id") String member_id,@Param("cnt") String cnt);
+	public Integer profilecnt(String member_id);
 
 	public void follow(Map<String, String> paramMap);
 	public void unfollow(Map<String, String> paramMap);
