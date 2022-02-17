86c7084928457cb3a2fe43a623bf64849aba8bbe
ung <ung@localhost>
2022-02-17 16:56:38 +0900

    파일업로드 이미지 crop기능 구현 초기단계 ksw 22.02.17 16:56

--- src/main/java/com/mypatch/www/board/controller/BoardController.java
+++ src/main/java/com/mypatch/www/board/controller/BoardController.java
@@ -1,22 +1,35 @@
 package com.mypatch.www.board.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mypatch.www.board.domain.BoardAttachDTO;
+
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
-@Log4j
 @RequestMapping("/board")
+@Log4j
 public class BoardController {
 	
 	private String uploadPath = "/Users/ung/IDE/workspace/mypatch/src/main/webapp/resources/fileUpload";
@@ -45,11 +58,15 @@ public class BoardController {
 	
 	// 페이지 특성상 ajax비동기 통신으로 첨부파일 처리하기로 함
 	@PostMapping(value = "/fileUpload", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public void uploadFile(MultipartFile[] uploadFile) {
+	@ResponseBody
+	public ResponseEntity<List<BoardAttachDTO>> uploadFile(MultipartFile[] uploadFile) {
 		
+		List<BoardAttachDTO> list = new ArrayList<BoardAttachDTO>();
 		log.info("fileUpload ajax POST--------------------");
 		
 		String uploadFolder = "/Users/ung/IDE/workspace/mypatch/src/main/webapp/resources/fileUpload/board";
+		
+		String uploadFolderPath = getFolder();
 		
 		// 첨부파일 폴더생성
 		File uploadPath = new File(uploadFolder, getFolder());
@@ -64,6 +81,8 @@ public class BoardController {
 			
 			log.info("--------------------");
 			
+			BoardAttachDTO boardAttachDTO = new BoardAttachDTO();
+			
 			log.info("Upload FIle Name : " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size : " + multipartFile.getSize());
 			
@@ -71,6 +90,7 @@ public class BoardController {
 			
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("/") + 1);
 			log.info("only file name : " + uploadFileName);
+			boardAttachDTO.setBoardattach_filename(uploadFileName);
 			
 			UUID uuid = UUID.randomUUID();
 			
@@ -78,19 +98,75 @@ public class BoardController {
 			log.info("BoardController uploadFile uuid + uploadFileName : " + uploadFileName); // 확인용이라 나중에 지워도 상관없음
 			
 			// File saveFile = new File(uploadFolder, uploadFileName); 테스트용 업로드 폴더
-			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				
+				boardAttachDTO.setBoardattach_uuid(uuid.toString());
+				boardAttachDTO.setBoardattach_path(uploadFolderPath);
+				log.info("boardAttachDTO setBoardattach_path : " + boardAttachDTO.getBoardattach_path());
+				
+				if (attachFileTypeCheck(saveFile)) {
+					
+					boardAttachDTO.setImageORvideo(true);
+					
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 400, 400);
+					
+					thumbnail.close();
+				}
+				
+				// 리스트 추가
+				list.add(boardAttachDTO);
 				
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			}
 			
 		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+		
+		log.info("BoardController getFile fileName : " + fileName);
+		
+		File file = new File("/Users/ung/IDE/workspace/mypatch/src/main/webapp/resources/fileUpload/board" + "/" + fileName);
+		
+		log.info("BoardController getFile file : " + file);
+		
+		ResponseEntity<byte[]> result = null;
+		
+		try {
+			
+			HttpHeaders header = new HttpHeaders();
+			
+			header.add("Content-Type", Files.probeContentType(file.toPath()));
+			result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			 e.printStackTrace();
+		}
+		return result;
+	}
 	
+	private boolean attachFileTypeCheck(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			
+			return contentType.startsWith("image");
+					
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
 	
 }
